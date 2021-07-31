c575b0727b31bc87307961cfc4341e9f250ffa7d
Octo5Eze <oeze@octo5.co>
2021-07-31 23:59:33 +0100

    seo

--- resources/lang/es/seo.php
+++ resources/lang/es/seo.php
@@ -8,6 +8,6 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	*/
-	"description" => "Dyskova is Africa's first subscription based content-on-demand (COD) platform, that allows users watch exclusive videos and content from their favorite celebrities and creators. ",
+	"description" => "Dyskova is Africa's first subscription based content-on-demand (COD) platform,  that allows users watch exclusive videos and content from their favorite celebrities and creators. ",
 	"keywords" => "donaciones,soporte,creadores,sponzy,suscripción,contenido",
 );
